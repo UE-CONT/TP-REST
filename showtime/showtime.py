6415ef25ceb83ffa2581327d4416950599d686b3
MaximeBrt <maxime.baraton92@gmail.com>
2023-09-20 11:02:27 +0200

    Update showtime.py

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -14,6 +14,18 @@ with open('{}/databases/times.json'.format("."), "r") as jsf:
 def home():
    return "<h1 style='color:blue'>Welcome to the Showtime service!</h1>"
 
+@app.route("/showtimes", methods=["GET"])
+def get_schedule():
+    return make_response(jsonify(schedule), 200)
+
+@app.route("/showmovies/<date>", methods=["GET"])
+def get_movies_bydaate(date):
+    for time in schedule:
+        if str(time["date"]) == str(date):
+            res = make_response(jsonify(time), 200)
+            return res
+    return make_response(jsonify({"error": "bad input parameter"}), 400)
+
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
    app.run(host=HOST, port=PORT)
